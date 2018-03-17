48412ea4685c09c4b0a95183401836d4adf9b52a
Andrei Ryzhkov <spinor72@gmail.com>
2018-03-18 01:56:57 +0300

    Fixed missing -y flag

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -8,7 +8,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 apt update
 
 # install and run
-apt install mongodb-org
+apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
 # check status
