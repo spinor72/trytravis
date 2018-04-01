c598d65cdde33424d5680499af8f9f1014e7814c
Andrei Ryzhkov <spinor72@gmail.com>
2018-04-01 12:56:30 +0300

    Homework-8. Prepare (set count and remove lb)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,12 @@
-# output "app_external_ip" {
-#   value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
-# }
+output "app_external_ip" {
+  value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
+}
 
 output "app_external_addresses" {
   value = ["${google_compute_instance.app.*.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
 
-output "lb_external_ip" {
-  value = "${google_compute_forwarding_rule.default.ip_address}"
-}
+# output "lb_external_ip" {
+#   value = "${google_compute_forwarding_rule.default.ip_address}"
+# }
+
