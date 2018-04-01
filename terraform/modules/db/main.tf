27f5ec8e852573ea462ab8617781f37c8e1b904a
Andrei Ryzhkov <spinor72@gmail.com>
2018-04-01 16:56:01 +0300

    Homework-8. Using modules in terraform for db, app and firewall.

---
+++ terraform/modules/db/main.tf
@@ -0,0 +1,45 @@
+resource "google_compute_instance" "db" {
+  name         = "reddit-db"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-db"]
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "${var.db_disk_image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config = {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+}
+
+resource "google_compute_firewall" "firewall_mongo" {
+  name    = "allow-mongo-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["27017"]
+  }
+
+  # правило применимо к инстансам с тегом ...
+  target_tags = ["reddit-db"]
+
+  # порт будет доступен только для инстансов с тегом ...
+  source_tags = ["reddit-app"]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,7 @@
 output "app_external_ip" {
-  value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "${module.app.app_external_ip}"
 }
 
-output "app_external_addresses" {
-  value = ["${google_compute_instance.app.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+output "db_external_ip" {
+  value = "${module.db.db_external_ip}"
 }
-
-# output "lb_external_ip" {
-#   value = "${google_compute_forwarding_rule.default.ip_address}"
-# }
-
