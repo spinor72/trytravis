8032b02b0cc64e3a1a08814969aa282f1ffa152a
Andrei Ryzhkov <spinor72@gmail.com>
2018-04-01 16:13:49 +0300

    Homework-8. Created app and db base images with packer and splited terraform config.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,3 +28,13 @@ variable count {
   description = "Number of instances"
   default     = 1
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,13 @@
+resource "google_compute_firewall" "firewall_ssh" {
+  name        = "default-allow-ssh"
+  network     = "default"
+  description = "Allow SSH from anywhere (terraform)"
+  priority    = "65534"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
