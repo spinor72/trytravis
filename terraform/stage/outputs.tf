26d0da10862a4919a80993fdc4fa7603356171d2
Andrei Ryzhkov <spinor72@gmail.com>
2018-04-07 15:05:39 +0300

    Homework-10. Single playbook, single play.

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -5,3 +5,46 @@ output "app_external_ip" {
 output "db_external_ip" {
   value = "${module.db.db_external_ip}"
 }
+
+output "db_internal_ip" {
+  value = "${module.db.db_internal_ip}"
+}
+
+output "inventory_txt" {
+  value = <<EOF
+[app]
+appserver ansible_host="${module.app.app_external_ip}"
+
+[db]
+dbserver ansible_host="${module.db.db_external_ip}"
+
+EOF
+}
+
+output "inventory_json" {
+  value = {
+    "app" = {
+      "hosts" = [
+        "appserver",
+      ]
+    }
+
+    "db" = {
+      "hosts" = [
+        "dbserver",
+      ]
+    }
+
+    "_meta" = {
+      "hostvars" = {
+        "appserver" = {
+          "ansible_host" = "${module.app.app_external_ip}"
+        }
+
+        "dbserver" = {
+          "ansible_host" = "${module.db.db_external_ip}"
+        }
+      }
+    }
+  }
+}
