48ac879c5caa0b24e6a6db1e33c3e9d047716ede
Andrei Ryzhkov <spinor72@gmail.com>
2018-04-10 21:17:25 +0300

    Removed *.sh from root of repo.

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# This script deploys reddit application.
-# ruby, bundle and mongodb have to be installed before
-#
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit
-bundle install
-sudo -u appuser puma -d
--- reddit-app-cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# delete firewall rule and VM silently
-gcloud compute firewall-rules delete default-puma-server --quiet
-gcloud compute instances delete reddit-app --quiet
